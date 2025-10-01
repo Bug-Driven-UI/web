855061fcba1f48aec1a0e685d8b1c89d1e3f39ab
vitrivdolkom <vdy0555@gmail.com>
2025-10-01 17:24:24 +0700

    fix

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -3,8 +3,14 @@ import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
 export const instance = axios.create({
-  // eslint-disable-next-line node/prefer-global/process
-  baseURL: typeof window !== 'undefined' ? '/api' : process.env.NEXT_PUBLIC_BACKEND_INTERNAL_URL
+  baseURL:
+    // eslint-disable-next-line node/prefer-global/process
+    typeof window !== 'undefined' ? '/api' : `${process.env.NEXT_PUBLIC_BACKEND_INTERNAL_URL}/api`
+});
+
+instance.interceptors.request.use((config) => {
+  console.log('## instance', JSON.stringify(config, null, 2));
+  return config;
 });
 
 export const getInstance = <T>(
