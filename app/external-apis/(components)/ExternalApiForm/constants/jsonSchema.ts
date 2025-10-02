071a591076b02265b8d855bae49e5725c5251261
vitrivdolkom <vdy0555@gmail.com>
2025-10-02 10:01:23 +0700

    fix json schema for external api schema

--- app/external-apis/(components)/ExternalApiForm/constants/jsonSchema.ts
+++ app/external-apis/(components)/ExternalApiForm/constants/jsonSchema.ts
@@ -2,213 +2,156 @@ import type { JSONSchema7 } from 'json-schema';
 
 export const EXTERNAL_API_SCHEMA_JSON_SCHEMA: JSONSchema7 = {
   $schema: 'http://json-schema.org/draft-07/schema#',
-  title: 'Schema (no-$ref, depth=2)',
-  description:
-    'Универсальная схема типов (object, array, string, number) без $ref. Поддерживается вложенность object/array до 2 уровней.',
-  oneOf: [
-    {
-      type: 'object',
-      additionalProperties: false,
-      required: ['type', 'properties'],
-      properties: {
-        type: { const: 'object' },
-        properties: {
-          type: 'object',
-          description: 'Словарь свойств объекта (имя поля -> схема).',
-          additionalProperties: {
-            oneOf: [
-              {
-                type: 'object',
-                additionalProperties: false,
-                required: ['type', 'properties'],
-                properties: {
-                  type: { const: 'object' },
-                  properties: {
-                    type: 'object',
-                    description: 'Словарь свойств объекта (имя поля -> схема).',
-                    additionalProperties: {
-                      oneOf: [
-                        {
-                          type: 'object',
-                          additionalProperties: false,
-                          required: ['type'],
-                          properties: {
-                            type: { const: 'string' }
-                          }
-                        },
-                        {
-                          type: 'object',
-                          additionalProperties: false,
-                          required: ['type'],
-                          properties: {
-                            type: { const: 'number' }
-                          }
-                        }
-                      ]
-                    }
-                  }
-                }
-              },
-              {
-                type: 'object',
-                additionalProperties: false,
-                required: ['type', 'items'],
-                properties: {
-                  type: { const: 'array' },
-                  items: {
-                    oneOf: [
-                      {
-                        type: 'object',
-                        additionalProperties: false,
-                        required: ['type'],
-                        properties: {
-                          type: { const: 'string' }
-                        }
-                      },
-                      {
-                        type: 'object',
-                        additionalProperties: false,
-                        required: ['type'],
-                        properties: {
-                          type: { const: 'number' }
-                        }
-                      }
-                    ]
-                  }
-                }
-              },
-              {
-                type: 'object',
-                additionalProperties: false,
-                required: ['type'],
-                properties: {
-                  type: { const: 'string' }
-                }
-              },
-              {
-                type: 'object',
-                additionalProperties: false,
-                required: ['type'],
-                properties: {
-                  type: { const: 'number' }
-                }
-              }
-            ]
-          }
-        }
-      }
+  $id: 'http://json-schema.org/draft-07/schema#',
+  title: 'Core schema meta-schema',
+  definitions: {
+    schemaArray: {
+      type: 'array',
+      minItems: 1,
+      items: { $ref: '#' }
     },
-    {
-      type: 'object',
-      additionalProperties: false,
-      required: ['type', 'items'],
-      properties: {
-        type: { const: 'array' },
-        items: {
-          oneOf: [
-            {
-              type: 'object',
-              additionalProperties: false,
-              required: ['type', 'properties'],
-              properties: {
-                type: { const: 'object' },
-                properties: {
-                  type: 'object',
-                  description: 'Словарь свойств объекта (имя поля -> схема).',
-                  additionalProperties: {
-                    oneOf: [
-                      {
-                        type: 'object',
-                        additionalProperties: false,
-                        required: ['type'],
-                        properties: {
-                          type: { const: 'string' }
-                        }
-                      },
-                      {
-                        type: 'object',
-                        additionalProperties: false,
-                        required: ['type'],
-                        properties: {
-                          type: { const: 'number' }
-                        }
-                      }
-                    ]
-                  }
-                }
-              }
-            },
-            {
-              type: 'object',
-              additionalProperties: false,
-              required: ['type', 'items'],
-              properties: {
-                type: { const: 'array' },
-                items: {
-                  oneOf: [
-                    {
-                      type: 'object',
-                      additionalProperties: false,
-                      required: ['type'],
-                      properties: {
-                        type: { const: 'string' }
-                      }
-                    },
-                    {
-                      type: 'object',
-                      additionalProperties: false,
-                      required: ['type'],
-                      properties: {
-                        type: { const: 'number' }
-                      }
-                    }
-                  ]
-                }
-              }
-            },
-            {
-              type: 'object',
-              additionalProperties: false,
-              required: ['type'],
-              properties: {
-                type: { const: 'string' }
-              }
-            },
-            {
-              type: 'object',
-              additionalProperties: false,
-              required: ['type'],
-              properties: {
-                type: { const: 'number' }
-              }
-            }
-          ]
-        }
-      }
+    nonNegativeInteger: {
+      type: 'integer',
+      minimum: 0
     },
-    {
-      type: 'object',
-      additionalProperties: false,
-      required: ['type'],
-      properties: {
-        type: { const: 'string' }
-      }
+    nonNegativeIntegerDefault0: {
+      allOf: [{ $ref: '#/definitions/nonNegativeInteger' }, { default: 0 }]
     },
-    {
-      type: 'object',
-      additionalProperties: false,
-      required: ['type'],
-      properties: {
-        type: { const: 'number' }
-      }
+    simpleTypes: {
+      enum: ['array', 'boolean', 'integer', 'null', 'number', 'object', 'string']
     },
-    {
-      type: 'object',
-      additionalProperties: false,
-      required: ['type'],
-      properties: {
-        type: { const: 'boolean' }
-      }
+    stringArray: {
+      type: 'array',
+      items: { type: 'string' },
+      uniqueItems: true,
+      default: []
     }
-  ]
+  },
+  type: ['object', 'boolean'],
+  properties: {
+    $id: {
+      type: 'string',
+      format: 'uri-reference'
+    },
+    $schema: {
+      type: 'string',
+      format: 'uri'
+    },
+    $ref: {
+      type: 'string',
+      format: 'uri-reference'
+    },
+    $comment: {
+      type: 'string'
+    },
+    title: {
+      type: 'string'
+    },
+    description: {
+      type: 'string'
+    },
+    default: true,
+    readOnly: {
+      type: 'boolean',
+      default: false
+    },
+    writeOnly: {
+      type: 'boolean',
+      default: false
+    },
+    examples: {
+      type: 'array',
+      items: true
+    },
+    multipleOf: {
+      type: 'number',
+      exclusiveMinimum: 0
+    },
+    maximum: {
+      type: 'number'
+    },
+    exclusiveMaximum: {
+      type: 'number'
+    },
+    minimum: {
+      type: 'number'
+    },
+    exclusiveMinimum: {
+      type: 'number'
+    },
+    maxLength: { $ref: '#/definitions/nonNegativeInteger' },
+    minLength: { $ref: '#/definitions/nonNegativeIntegerDefault0' },
+    pattern: {
+      type: 'string',
+      format: 'regex'
+    },
+    additionalItems: { $ref: '#' },
+    items: {
+      anyOf: [{ $ref: '#' }, { $ref: '#/definitions/schemaArray' }],
+      default: true
+    },
+    maxItems: { $ref: '#/definitions/nonNegativeInteger' },
+    minItems: { $ref: '#/definitions/nonNegativeIntegerDefault0' },
+    uniqueItems: {
+      type: 'boolean',
+      default: false
+    },
+    contains: { $ref: '#' },
+    maxProperties: { $ref: '#/definitions/nonNegativeInteger' },
+    minProperties: { $ref: '#/definitions/nonNegativeIntegerDefault0' },
+    required: { $ref: '#/definitions/stringArray' },
+    additionalProperties: { $ref: '#' },
+    definitions: {
+      type: 'object',
+      additionalProperties: { $ref: '#' },
+      default: {}
+    },
+    properties: {
+      type: 'object',
+      additionalProperties: { $ref: '#' },
+      default: {}
+    },
+    patternProperties: {
+      type: 'object',
+      additionalProperties: { $ref: '#' },
+      propertyNames: { format: 'regex' },
+      default: {}
+    },
+    dependencies: {
+      type: 'object',
+      additionalProperties: {
+        anyOf: [{ $ref: '#' }, { $ref: '#/definitions/stringArray' }]
+      }
+    },
+    propertyNames: { $ref: '#' },
+    const: true,
+    enum: {
+      type: 'array',
+      items: true,
+      minItems: 1,
+      uniqueItems: true
+    },
+    type: {
+      anyOf: [
+        { $ref: '#/definitions/simpleTypes' },
+        {
+          type: 'array',
+          items: { $ref: '#/definitions/simpleTypes' },
+          minItems: 1,
+          uniqueItems: true
+        }
+      ]
+    },
+    format: { type: 'string' },
+    contentMediaType: { type: 'string' },
+    contentEncoding: { type: 'string' },
+    if: { $ref: '#' },
+    then: { $ref: '#' },
+    else: { $ref: '#' },
+    allOf: { $ref: '#/definitions/schemaArray' },
+    anyOf: { $ref: '#/definitions/schemaArray' },
+    oneOf: { $ref: '#/definitions/schemaArray' },
+    not: { $ref: '#' }
+  },
+  default: true
 };
